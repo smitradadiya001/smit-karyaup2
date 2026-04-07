b81826eb83e2ca415cc687f7749c104b5de9dd29
karyaup-8999 <karyaaup@gmail.com>
2026-04-07 18:03:46 +0530

    new repo

--- src/components/AllInOne.jsx
+++ src/components/AllInOne.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion     } from 'framer-motion';
 import {
     CheckSquare, BarChart3, Zap,
     LayoutDashboard, Clock,
